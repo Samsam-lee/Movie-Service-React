bac081bd487231e6f07ba965a16eb85be37c9acc
이승형 <shlee@MacBook-Pro.local>
2020-09-13 02:47:03 +0900

    css 추가

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {HashRouter, Route} from "react-router-dom";
 import About from "./routes/About";
 import Home from "./routes/Home";
 import Navigation from "./components/Navigation";
+import "./App.css";
 
 function App(){
   return <HashRouter>
