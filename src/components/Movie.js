4d34d4d25638da5916b9ec0ed9fde68295b79fd7
이승형 <shlee@MacBook-Pro.local>
2020-09-13 04:19:58 +0900

    리다이렉팅

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,17 +1,27 @@
 import React from 'react';
+import {Link} from "react-router-dom";
 import PropTypes from 'prop-types';
 import "./Movie.css";
 
-function Movie({year, title, summary, poster, genres}){
-    return <div className="movie">
-        <img src={poster} alt={title} title={title}/>
-        <div>
-            <h3 className="movie_title">{title}</h3>
-            <h5 className="movie_year">{year}</h5>
-            <ul className="genres">{genres.map((genre, index) => <li key={index} className="genres_genre">{genre}</li>)}</ul>
-            <p className="movie_summary">{summary.slice(0, 200)}...</p>
+function Movie({id, year, title, summary, poster, genres}){
+    return (
+        <div className="movie">
+            <Link to={{
+                pathname: `/movie/${id}`,
+                state: {
+                    year, title, summary, poster, genres
+                }
+            }}>
+                <img src={poster} alt={title} title={title} />
+                <div>
+                    <h3 className="movie_title">{title}</h3>
+                    <h5 className="movie_year">{year}</h5>
+                    <ul className="movie_genres">{genres.map((genre, index) => <li key={index}>{genre}</li>)}</ul>
+                    <p className="movie_summary">{summary.slice(0, 200)}...</p>
+                </div>
+            </Link>
         </div>
-    </div>;
+    );
 }
 
 Movie.propTypes = {
