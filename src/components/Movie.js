3f9d2a214969370d0f71c288f387d3473da4dc95
이승형 <shlee@MacBook-Pro.local>
2020-09-12 21:38:49 +0900

    movie_app_#5

---
+++ src/components/Movie.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import "./Movie.css";
+
+function Movie({year, title, summary, poster, genres}){
+    return <div className="movie">
+        <img src={poster} alt={title} title={title}/>
+        <div>
+            <h3 className="movie_title">{title}</h3>
+            <h5 className="movie_year">{year}</h5>
+            <ul className="genres">{genres.map((genre, index) => <li key={index} className="genres_genre">{genre}</li>)}</ul>
+            <p className="movie_summary">{summary.slice(0, 200)}...</p>
+        </div>
+    </div>;
+}
+
+Movie.propTypes = {
+    id: PropTypes.number.isRequired,
+    year: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    summary: PropTypes.string.isRequired,
+    poster: PropTypes.string.isRequired,
+    genres: PropTypes.arrayOf(PropTypes.string).isRequired
+}
+
+export default Movie;
